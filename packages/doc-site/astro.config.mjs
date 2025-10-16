a0cfa70830ad69d2397cb418231c51009bb05a83
Altinok Darici <ad@buildxn.com>
2025-10-15 22:40:07 -0700

    fix: doc site base

--- packages/doc-site/astro.config.mjs
+++ packages/doc-site/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://buildxn.github.io',
+	base: '/wapix',
 	integrations: [
 		starlight({
 			title: 'wapix',
